c58ed67bded2638f472cdc37de4ab2521a16bdbe
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-10-16 14:40:35 +0200

    Package update and new start script

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-export { default as Modal } from "./components/modal/Modal";
 export { default as Button } from "./components/button/Button";
+export { default as TextButton } from "./components/button/TextButton";
+export { default as OutlineButton } from "./components/button/OutlineButton";
+export {
+  default as ContainedButton
+} from "./components/button/ContainedButton";
+export { default as ToggleButton } from "./components/button/ToggleButton";
+export { default as FabButton } from "./components/button/FabButton";
+export { default as Modal } from "./components/modal/Modal";
+export { default as Star } from "./components/star/Star";
+export { default as Stars } from "./components/stars/Stars";
+export { default as SVG } from "./components/svg/SVG";
+export { default as Text } from "./components/text/Text";
+export { cross } from "./components/svgs/cross";
+export { eyeHide } from "./components/svgs/eye-hide";
+export { eyeShow } from "./components/svgs/eye-show";
+export { plus } from "./components/svgs/plus";
+export { rocket } from "./components/svgs/rocket";
+export { rocketColored } from "./components/svgs/rocketColored";
