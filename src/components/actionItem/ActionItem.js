397daad2b1219f7014fd50ebb65bc7fcd7c1e2e1
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-10-22 00:33:56 +0200

    ActionItem as an extension of button for menu

---
+++ src/components/actionItem/ActionItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import SVG from "../svg/SVG";
+import { Button } from "../button/Button";
+
+const Item = styled(Button)`
+  background-color: inherit;
+  border: none;
+  padding: 2rem;
+  margin: 0;
+  min-width: 3rem;
+
+  :hover {
+    background-color: ${p =>
+      p.backgroundColorHover ? `${p.backgroundColorHover}33` : "inherit"};
+  }
+  :focus {
+    background-color: ${p =>
+      p.backgroundColorHover ? `${p.backgroundColorHover}88` : "inherit"};
+  }
+  :active {
+    background-color: ${p =>
+      p.backgroundColorHover ? `${p.backgroundColorHover}BB` : "inherit"};
+  }
+`;
+
+const ActionItem = ({ backgroundColorHover, color, svg, noBorder }) => (
+  <Item backgroundColorHover={backgroundColorHover} noBorder={noBorder}>
+    <SVG {...svg} color={color} />
+  </Item>
+);
+
+export default ActionItem;
