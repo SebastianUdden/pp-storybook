56a7f38bb98ebc7196698f2d61d7a157a73ef2e7
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-10-26 23:17:53 +0200

    memoize sections to improve performance

--- src/components/searchSections/SearchSections.js
+++ src/components/searchSections/SearchSections.js
@@ -20,59 +20,63 @@ const Paragraph = styled.p`
   color: white;
 `;
 
-const SearchSections = ({
-  sections = [],
-  searchValue = "",
-  selectedSection = "",
-  foregroundColor = "orange",
-  backgroundColor = "transparent"
-}) => {
-  return (
-    <Sections>
-      {selectedSection
-        ? sections
-            .filter(
-              section =>
-                section.title.toLowerCase() === selectedSection.toLowerCase()
-            )
-            .map(section => {
-              return (
+const SearchSections = React.memo(
+  ({
+    sections = [],
+    searchValue = "",
+    selectedSection = "",
+    foregroundColor = "orange",
+    backgroundColor = "transparent"
+  }) => {
+    return (
+      <Sections>
+        {selectedSection
+          ? sections
+              .filter(
+                section =>
+                  section.title.toLowerCase() === selectedSection.toLowerCase()
+              )
+              .map(section => {
+                return (
+                  <Section key={section.id}>
+                    <Title>{section.title}</Title>
+                    <Paragraph>{section.paragraph}</Paragraph>
+                  </Section>
+                );
+              })
+          : sections
+              .filter(
+                section =>
+                  section.paragraph
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase()) ||
+                  section.title
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase())
+              )
+              .map(section => (
                 <Section key={section.id}>
-                  <Title>{section.title}</Title>
-                  <Paragraph>{section.paragraph}</Paragraph>
+                  <Title>
+                    <HighlightedText
+                      text={section.title}
+                      highlight={searchValue}
+                      foregroundColor={foregroundColor}
+                      backgroundColor={backgroundColor}
+                    />
+                  </Title>
+                  <Paragraph>
+                    <HighlightedText
+                      text={section.paragraph}
+                      highlight={searchValue}
+                      foregroundColor={foregroundColor}
+                      backgroundColor={backgroundColor}
+                    />
+                  </Paragraph>
                 </Section>
-              );
-            })
-        : sections
-            .filter(
-              section =>
-                section.paragraph
-                  .toLowerCase()
-                  .includes(searchValue.toLowerCase()) ||
-                section.title.toLowerCase().includes(searchValue.toLowerCase())
-            )
-            .map(section => (
-              <Section key={section.id}>
-                <Title>
-                  <HighlightedText
-                    text={section.title}
-                    highlight={searchValue}
-                    foregroundColor={foregroundColor}
-                    backgroundColor={backgroundColor}
-                  />
-                </Title>
-                <Paragraph>
-                  <HighlightedText
-                    text={section.paragraph}
-                    highlight={searchValue}
-                    foregroundColor={foregroundColor}
-                    backgroundColor={backgroundColor}
-                  />
-                </Paragraph>
-              </Section>
-            ))}
-    </Sections>
-  );
-};
+              ))}
+      </Sections>
+    );
+  }
+);
 
 export default SearchSections;
