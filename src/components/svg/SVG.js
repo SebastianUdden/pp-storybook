e52e3364171949f86dcb3ec3b123560b002b8b61
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-11-01 14:12:23 +0100

    remove redundant return statement

--- src/components/svg/SVG.js
+++ src/components/svg/SVG.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-const SVG = ({ color = "white", viewBox, path, size = 24 }) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size || "24"}
-      height={size || "24"}
-      viewBox={viewBox}
-      fill={color || undefined}
-    >
-      {path}
-    </svg>
-  );
-};
+const SVG = ({ color = "white", viewBox, path, size = 24 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size || "24"}
+    height={size || "24"}
+    viewBox={viewBox}
+    fill={color || undefined}
+  >
+    {path}
+  </svg>
+);
 
 export default SVG;
