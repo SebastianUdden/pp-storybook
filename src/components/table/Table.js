4a16f4c83804fe2bcf1ce572751da1ccd83dca5b
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-11-01 16:08:06 +0100

    table update

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import { DP_TYPES } from "../../constants/theme";
+import { DP_TYPES, MAIN_THEME } from "../../constants/theme";
 import { DEFAULT_FONT } from "../../constants/font";
+import { getLinkValues } from "../../utils/markdownParsers";
+import { getNumberWithSpaces, round } from "../../utils/numberParsers";
 
 const Wrapper = styled.table`
   font-family: ${DEFAULT_FONT.family};
@@ -13,6 +15,8 @@ const Wrapper = styled.table`
 `;
 const THead = styled.thead`
   border-bottom: 2px solid ${p => p.alternateColor};
+  background-color: ${p => p.headingBackgroundColor};
+  color: ${p => p.headingForegroundColor};
   padding: 2rem;
 `;
 const TBody = styled.tbody`
@@ -24,33 +28,88 @@ const TH = styled.th`
   text-align: left;
   padding: 0.5rem 1rem;
   background-color: ${p => p.backgroundColor}22;
+  text-align: ${p => (p.alignRight ? "right" : "left")};
 `;
-const TR = styled.tr``;
+const TR = styled.tr`
+  :hover {
+    cursor: pointer;
+    box-shadow: ${DP_TYPES.DP6};
+
+    td {
+      color: ${p => p.headingForegroundColor};
+      background-color: ${p => p.headingBackgroundColor}BB !important;
+    }
+  }
+`;
 const TD = styled.td`
   padding: 0.5rem 1rem;
+  text-align: ${p => (p.alignRight ? "right" : "left")};
+`;
+
+const Link = styled.a`
+  font-family: ${DEFAULT_FONT.family};
+  color: ${p => p.foregroundColor};
+  text-decoration: none;
+  font-weight: 800;
 `;
 
 const Table = ({
   headings,
   data,
+  headingForegroundColor = "#000000",
+  headingBackgroundColor = "#DDDDDD",
   backgroundColor = "#DDDDDD",
   alternateColor = "#BBBBBB"
 }) => {
   return (
     <Wrapper backgroundColor={backgroundColor}>
-      <THead alternateColor={alternateColor}>
+      <THead
+        headingForegroundColor={headingForegroundColor}
+        headingBackgroundColor={headingBackgroundColor}
+        alternateColor={alternateColor}
+      >
         <TR>
           {headings.map(heading => (
-            <TH backgroundColor={backgroundColor}>{heading}</TH>
+            <TH
+              backgroundColor={backgroundColor}
+              alignRight={heading.alignRight}
+            >
+              {heading.title}
+            </TH>
           ))}
         </TR>
       </THead>
       <TBody alternateColor={alternateColor}>
         {data.rows.map(row => (
-          <TR>
-            {row.cells.map(cell => (
-              <TD>{cell}</TD>
-            ))}
+          <TR
+            headingBackgroundColor={headingBackgroundColor}
+            headingBackgroundColor={headingBackgroundColor}
+          >
+            {row.cells.map(cell => {
+              let displayCell;
+              const isNumber = typeof cell === "number";
+              const isString = typeof cell === "string";
+              if (isString) {
+                const linkValues = getLinkValues(cell);
+                displayCell = linkValues.length ? (
+                  <Link
+                    foregroundColor={MAIN_THEME.BLACK.color.background}
+                    href={linkValues[1]}
+                    alt={linkValues[0]}
+                    target="_blank"
+                  >
+                    {linkValues[0]}
+                  </Link>
+                ) : (
+                  cell
+                );
+              } else if (isNumber) {
+                displayCell = getNumberWithSpaces(round(cell));
+              } else {
+                displayCell = cell;
+              }
+              return <TD alignRight={isNumber}>{displayCell}</TD>;
+            })}
           </TR>
         ))}
       </TBody>
