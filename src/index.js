9f6d4b4352985b360e164f73342eb47b0f1d4fb6
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-11-01 14:14:42 +0100

    Add additional imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export { Button } from "./components/button/Button";
+export { default as ActionItem } from "./components/actionItem/ActionItem";
+export { default as AppBarTop } from "./components/appBarTop/AppBarTop";
 export { default as TextButton } from "./components/button/TextButton";
 export { default as OutlineButton } from "./components/button/OutlineButton";
 export {
@@ -6,11 +8,32 @@ export {
 } from "./components/button/ContainedButton";
 export { default as ToggleButton } from "./components/button/ToggleButton";
 export { default as FabButton } from "./components/button/FabButton";
+export { default as Card } from "./components/card/Card";
+export { default as ColoredBox } from "./components/coloredBox/ColoredBox";
+export {
+  default as HighlightedText
+} from "./components/highlightedText/HighlightedText";
 export { default as Modal } from "./components/modal/Modal";
+export {
+  default as NavigationDrawer
+} from "./components/navigationDrawer/NavigationDrawer";
+export {
+  default as NavigationLink
+} from "./components/navigationLink/NavigationLink";
+export {
+  default as OverflowMenu
+} from "./components/overflowMenu/OverflowMenu";
+export { default as Search } from "./components/search/Search";
+export {
+  default as SearchSections
+} from "./components/searchSections/SearchSections";
+export {
+  default as ToggleSwitch
+} from "./components/toggleSwitch/ToggleSwitch";
 export { default as Star } from "./components/star/Star";
 export { default as Stars } from "./components/stars/Stars";
 export { default as SVG } from "./components/svg/SVG";
-export { Text } from "./components/text/Text";
+export { default as Text } from "./components/text/Text";
 
 export * from "./svgs/actions";
 export * from "./svgs/alert";
