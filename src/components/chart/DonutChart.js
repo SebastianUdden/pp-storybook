d10a81477b6cb7dac7475ef1ea7ced9270f684ca
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-11-05 16:14:59 +0100

    Pie and donut chart refactor

--- src/components/chart/DonutChart.js
+++ src/components/chart/DonutChart.js
@@ -1,41 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 
-const Pie = styled.circle`
-  fill: ${p => p.color};
+const Donut = styled.circle`
+  fill: none;
   stroke: ${p => p.color};
-  stroke-width: 0;
+  stroke-width: 12;
   transform: rotate(-90deg);
   transform-origin: 50% 50%;
 `;
 // 2 * π * R  = C
 // 2 * π * 40 ≈ 251.327
-const PieFill = styled.circle`
+const DonutFill = styled.circle`
   fill: none;
   stroke: ${p => p.color};
-  stroke-width: 50;
-  stroke-dasharray: 157.07;
-  stroke-dashoffset: ${p => `calc(157.07 * (1 - ${p.fillPercentage / 100}))`};
+  stroke-width: 12;
+  stroke-dasharray: 276.46;
+  stroke-dashoffset: ${p => `calc(276.46 * (1 - ${p.fillPercentage / 100}))`};
   transition: stroke-dashoffset 0.3s ease;
   transform: rotate(-90deg);
   transform-origin: 50% 50%;
 `;
 
-const PieChart = ({
-  fillPercentage = 50,
-  color = "#000000",
-  alternateColor = "#ffffff"
-}) => (
+const DonutChart = ({ fillPercentage = 50, color, alternateColor }) => (
   <>
-    <Pie cx="50" cy="50" r="50" color={color} />
-    <PieFill
+    <Donut cx="50" cy="50" r="44" color={color} />
+    <DonutFill
       cx="50"
       cy="50"
-      r="25"
+      r="44"
       fillPercentage={fillPercentage}
       color={alternateColor}
     />
   </>
 );
 
-export default PieChart;
+export default DonutChart;
