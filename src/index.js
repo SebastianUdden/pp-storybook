899f6b909e55eda048170c4cddc7431162342300
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-11-08 19:12:24 +0100

    updated version and exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+export { countLines } from "./utils/textParsers";
+export { isEquivalent } from "./utils/object";
+export { getNumberWithSpaces, round, roundTwoDec } from "./utils/numberParser";
+export { getPercentageOfTotal, getRelativePosition, sum } from "./utils/math";
+export { getLinkValues } from "./utils/markdownParsers";
 export { Button } from "./components/button/Button";
 export { default as ActionItem } from "./components/actionItem/ActionItem";
 export { default as AppBarTop } from "./components/appBarTop/AppBarTop";
@@ -9,7 +14,13 @@ export {
 export { default as ToggleButton } from "./components/button/ToggleButton";
 export { default as FabButton } from "./components/button/FabButton";
 export { default as Card } from "./components/card/Card";
+export { default as Chart } from "./components/chart/Chart";
+export { default as Chips } from "./components/chips/Chips";
 export { default as ColoredBox } from "./components/coloredBox/ColoredBox";
+export { default as DataCards } from "./components/dataCards/DataCards";
+export { default as Dropdown } from "./components/dropdown/Dropdown";
+export { default as Flex } from "./components/flex/Flex";
+export { default as Grid } from "./components/grid/Grid";
 export {
   default as HighlightedText
 } from "./components/highlightedText/HighlightedText";
@@ -23,6 +34,7 @@ export {
 export {
   default as OverflowMenu
 } from "./components/overflowMenu/OverflowMenu";
+export { default as Ripple } from "./components/ripple/Ripple";
 export { default as Search } from "./components/search/Search";
 export {
   default as SearchSections
@@ -33,7 +45,9 @@ export {
 export { default as Star } from "./components/star/Star";
 export { default as Stars } from "./components/stars/Stars";
 export { default as SVG } from "./components/svg/SVG";
+export { default as Table } from "./components/table/Table";
 export { default as Text } from "./components/text/Text";
+export { default as ToggleSwitch } from "./components/toggleSwitch/ToggleSwitch";
 
 export * from "./svgs/actions";
 export * from "./svgs/alert";
