fff5c54f892d2ea362b9e0a929ab520a25a32a14
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-11-14 18:22:47 +0100

    export new components

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ export { default as ActionItem } from "./components/actionItem/ActionItem";
 export { default as AppBarTop } from "./components/appBarTop/AppBarTop";
 export { default as TextButton } from "./components/button/TextButton";
 export { default as OutlineButton } from "./components/button/OutlineButton";
-export {
-  default as ContainedButton
-} from "./components/button/ContainedButton";
+export { default as ContainedButton } from "./components/button/ContainedButton";
+export { default as Breadcrumbs } from "./components/breadcrumbs/Breadcrumbs";
 export { default as ToggleButton } from "./components/button/ToggleButton";
 export { default as FabButton } from "./components/button/FabButton";
 export { default as Card } from "./components/card/Card";
@@ -21,32 +20,22 @@ export { default as DataCards } from "./components/dataCards/DataCards";
 export { default as Dropdown } from "./components/dropdown/Dropdown";
 export { default as Flex } from "./components/flex/Flex";
 export { default as Grid } from "./components/grid/Grid";
-export {
-  default as HighlightedText
-} from "./components/highlightedText/HighlightedText";
+export { default as Heading } from "./components/heading/Heading";
+export { default as HighlightedText } from "./components/highlightedText/HighlightedText";
 export { default as Modal } from "./components/modal/Modal";
-export {
-  default as NavigationDrawer
-} from "./components/navigationDrawer/NavigationDrawer";
-export {
-  default as NavigationLink
-} from "./components/navigationLink/NavigationLink";
-export {
-  default as OverflowMenu
-} from "./components/overflowMenu/OverflowMenu";
+export { default as MarkdownParser } from "./components/markdownParser/MarkdownParser";
+export { default as NavigationDrawer } from "./components/navigationDrawer/NavigationDrawer";
+export { default as NavigationLink } from "./components/navigationLink/NavigationLink";
+export { default as OverflowMenu } from "./components/overflowMenu/OverflowMenu";
 export { default as Ripple } from "./components/ripple/Ripple";
 export { default as Search } from "./components/search/Search";
-export {
-  default as SearchSections
-} from "./components/searchSections/SearchSections";
+export { default as SearchSections } from "./components/searchSections/SearchSections";
 export { default as Star } from "./components/star/Star";
 export { default as Stars } from "./components/stars/Stars";
 export { default as SVG } from "./components/svg/SVG";
 export { default as Table } from "./components/table/Table";
 export { default as Text } from "./components/text/Text";
-export {
-  default as ToggleSwitch
-} from "./components/toggleSwitch/ToggleSwitch";
+export { default as ToggleSwitch } from "./components/toggleSwitch/ToggleSwitch";
 
 export * from "./svgs/actions";
 export * from "./svgs/alert";
