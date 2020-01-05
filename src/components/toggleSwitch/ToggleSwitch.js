696350be24d5feed16ac7ae1300b6c0d17095e6c
Sebastian Uddén <sebastian.udden@danielwellington.com>
2020-01-06 00:11:46 +0100

    feat: size attribute for toggle switch

--- src/components/toggleSwitch/ToggleSwitch.js
+++ src/components/toggleSwitch/ToggleSwitch.js
@@ -2,11 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const Box = styled.label`
-  margin-left: 0.2rem;
+  margin-left: 5px;
   position: relative;
   display: inline-block;
-  width: 60px;
-  height: 34px;
+  width: ${p => p.size * 1.764}px;
+  height: ${p => p.size}px;
 `;
 
 const CheckBox = styled.input`
@@ -32,32 +32,35 @@ const Slider = styled.span`
   background-color: ${p => (p.checked ? p.backgroundColor : "#ccc")};
   -webkit-transition: 0.4s;
   transition: 0.4s;
-  border-radius: ${p => (p.rounded ? 34 : 0)}px;
+  border-radius: ${p => (p.rounded ? p.size : 0)}px;
 
   :before {
     border-radius: ${p => (p.rounded ? 50 : 0)}%;
     position: absolute;
     content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
+    height: ${p => p.size * 0.764}px;
+    width: ${p => p.size * 0.764}px;
+    left: ${p => p.size * 0.117}px;
+    bottom: ${p => p.size * 0.1}px;
     background-color: white;
     -webkit-transition: 0.4s;
     transition: 0.4s;
-    transform: ${p => (p.checked ? "translateX(26px)" : "translateX(0)")};
+    transform: ${p =>
+      p.checked ? `translateX(${p.size * 0.764}px)` : "translateX(0)"};
   }
 `;
 
 const ToggleSwitch = ({
   checked,
   onClick,
+  size = 34,
   rounded = true,
   backgroundColor = "#2196f3"
 }) => (
-  <Box>
+  <Box size={size}>
     <CheckBox type="checkbox" onClick={onClick} />
     <Slider
+      size={size}
       checked={checked}
       rounded={rounded}
       backgroundColor={backgroundColor}
